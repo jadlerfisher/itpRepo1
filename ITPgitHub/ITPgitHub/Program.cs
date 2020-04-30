5fa7e27094c1998bd4836f49cb13a91849c5036a
jadlerfisher <jadlerfisher@gmail.com>
2020-04-30 15:19:32 -0500

    First Push
    Welcome to the Git!

---
+++ ITPgitHub/ITPgitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ITPgitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Git!");
+        }
+    }
+}
